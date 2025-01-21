766b493b05df94b725699d23631bf6c1edfd572f
Turboman3000 <turboman@turboman.de>
2025-01-21 16:45:44 +0100

    refactor(CTFTeam): allow nullable leader UUID

--- src/main/java/de/turboman/ctf/CTFTeam.java
+++ src/main/java/de/turboman/ctf/CTFTeam.java
@@ -1,7 +1,8 @@
 package de.turboman.ctf;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.UUID;
 
-public record CTFTeam(String name, String color, ArrayList<UUID> players, UUID leader) {
+public record CTFTeam(String name, String color, ArrayList<UUID> players, @Nullable UUID leader) {
 }
