559c8fb3c2ce901739e28289ad9e299d528bccbf
Turboman3000 <turboman@turboman.de>
2025-01-21 16:42:20 +0100

    feat(CTFTeam): add leader field to CTFTeam record

--- src/main/java/de/turboman/ctf/CTFTeam.java
+++ src/main/java/de/turboman/ctf/CTFTeam.java
@@ -3,5 +3,5 @@ package de.turboman.ctf;
 import java.util.ArrayList;
 import java.util.UUID;
 
-public record CTFTeam(String name, String color, ArrayList<UUID> players) {
+public record CTFTeam(String name, String color, ArrayList<UUID> players, UUID leader) {
 }
