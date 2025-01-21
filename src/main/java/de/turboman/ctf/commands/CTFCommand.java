8ba88d3c54440307dc9904f44c0473eea9d13c3b
Turboman3000 <turboman@turboman.de>
2025-01-21 16:35:32 +0100

    feat(CTFCommand): Add team management and player addition features

--- src/main/java/de/turboman/ctf/commands/CTFCommand.java
+++ src/main/java/de/turboman/ctf/commands/CTFCommand.java
@@ -1,12 +1,13 @@
 package de.turboman.ctf.commands;
 
 import de.turboman.ctf.CTFTeam;
-import de.turboman.ctf.CaptureTheFlag;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static de.turboman.ctf.CaptureTheFlag.prefix;
+import static de.turboman.ctf.CaptureTheFlag.teamList;
 
 public class CTFCommand implements CommandExecutor, TabCompleter {
     private MiniMessage mm = MiniMessage.miniMessage();
@@ -30,21 +32,39 @@ public class CTFCommand implements CommandExecutor, TabCompleter {
                 case "create" -> {
                     if (args.length != 4) break;
 
-                    CaptureTheFlag.teamList.add(new CTFTeam(args[2], args[3], List.of()));
+                    teamList.add(new CTFTeam(args[2], args[3], List.of()));
                     sender.sendMessage(mm.deserialize(prefix + "<green>Team <gold>" + args[2] + "<green> created!"));
                 }
                 case "delete" -> {
                     if (args.length != 3) break;
 
-                    for (var t : CaptureTheFlag.teamList) {
+                    for (var t : teamList) {
                         if (t.name().equals(args[2])) {
-                            CaptureTheFlag.teamList.remove(t);
+                            teamList.remove(t);
                             break;
                         }
                     }
                 }
                 case "add" -> {
+                    if (args.length != 4) break;
 
+                    Player player = Bukkit.getPlayer(args[3]);
+
+                    for (var t : teamList) {
+                        if (t.players().contains(player.getUniqueId())) {
+                            sender.sendMessage(mm.deserialize(prefix + "<red>" + player.getName() + " is already in a team!"));
+                            break;
+                        }
+                    }
+
+                    for (var t : teamList) {
+                        if (t.name().equals(args[2])) {
+                            t.players().add(player.getUniqueId());
+                            sender.sendMessage(mm.deserialize(prefix + "<green> Added player <gold>" + player.getName() + "<green> to the Team: <gold>" + t.name()));
+                            player.sendMessage(mm.deserialize(prefix + "<green>You are now in Team: <gold>" + t.name()));
+                            break;
+                        }
+                    }
                 }
                 case "remove" -> {
 
@@ -58,7 +78,7 @@ public class CTFCommand implements CommandExecutor, TabCompleter {
                 case "list" -> {
                     sender.sendMessage(mm.deserialize(prefix + "<green>List of Teams:"));
 
-                    for (var t : CaptureTheFlag.teamList) {
+                    for (var t : teamList) {
                         sender.sendMessage(mm.deserialize(prefix + "<" + t.color() + ">" + t.name() + "<gray> (" + t.players().size() + ")"));
                     }
                 }
@@ -76,7 +96,8 @@ public class CTFCommand implements CommandExecutor, TabCompleter {
             output.add("team");
             output.add("start");
             output.add("stop");
-        } else if (args.length == 2 && args[0].equalsIgnoreCase("team")) {
+        } else if (args.length == 2
+                && args[0].equalsIgnoreCase("team")) {
             output.add("create");
             output.add("delete");
             output.add("add");
@@ -102,6 +123,18 @@ public class CTFCommand implements CommandExecutor, TabCompleter {
             output.add("light_purple");
             output.add("yellow");
             output.add("white");
+        } else if (args.length == 3
+                && args[0].equalsIgnoreCase("team")
+                && args[1].equalsIgnoreCase("add")) {
+            for (var t : teamList) {
+                output.add(t.name());
+            }
+        } else if (args.length == 4
+                && args[0].equalsIgnoreCase("team")
+                && args[1].equalsIgnoreCase("add")) {
+            for (var p : Bukkit.getOnlinePlayers()) {
+                output.add(p.getName());
+            }
         }
 
         return output;
