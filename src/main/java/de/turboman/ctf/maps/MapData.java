eba0ea641f099913a64f994cb3b63118f4954351
Turboman3000 <turboman@turboman.de>
2025-01-25 15:06:38 +0100

    refactor(MapManager, MapData): Optimize map decoration handling

--- src/main/java/de/turboman/ctf/maps/MapData.java
+++ src/main/java/de/turboman/ctf/maps/MapData.java
@@ -1,9 +1,9 @@
 package de.turboman.ctf.maps;
 
+import io.papermc.paper.datacomponent.item.MapDecorations;
 import org.bukkit.map.MapView;
 
-import java.util.ArrayList;
-import java.util.UUID;
+import java.util.HashMap;
 
-public record MapData(MapView view, ArrayList<UUID> unlockedFlags) {
+public record MapData(MapView view, HashMap<String, MapDecorations.DecorationEntry> decorations) {
 }
--- src/main/java/de/turboman/ctf/maps/MapManager.java
+++ src/main/java/de/turboman/ctf/maps/MapManager.java
@@ -1,5 +1,7 @@
 package de.turboman.ctf.maps;
 
+import io.papermc.paper.datacomponent.DataComponentTypes;
+import io.papermc.paper.datacomponent.item.MapDecorations;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -7,7 +9,6 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.MapMeta;
 import org.bukkit.map.MapView;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -26,14 +27,7 @@ public class MapManager {
         view.setUnlimitedTracking(true);
         view.setTrackingPosition(true);
 
-        /*
-         HashMap<String, MapDecorations.DecorationEntry> mapDeco = new HashMap<>();
-         mapDeco.put(UUID.randomUUID() + "", MapDecorations.decorationEntry(getDecoColor(t), player.getLocation().getBlockX(), player.getLocation().getBlockZ(), 0));
-
-         mapItem.setData(DataComponentTypes.MAP_DECORATIONS, MapDecorations.mapDecorations().putAll(mapDeco).build());
-         */
-
-        playerMaps.put(player.getUniqueId(), new MapData(view, new ArrayList<>()));
+        playerMaps.put(player.getUniqueId(), new MapData(view, new HashMap<>()));
     }
 
     public static ItemStack getMapItem(UUID uuid) {
@@ -43,6 +37,7 @@ public class MapManager {
 
         meta.setMapView(data.view());
 
+        mapItem.setData(DataComponentTypes.MAP_DECORATIONS, MapDecorations.mapDecorations().putAll(data.decorations()).build());
         mapItem.setItemMeta(meta);
 
         return mapItem;
