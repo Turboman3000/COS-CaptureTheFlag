684a1d48afc663991a0224d6124fd1f50078fd48
Turboman3000 <turboman@turboman.de>
2025-01-25 15:38:53 +0100

    feat(PlayerPlaceBlockEvent): Add flag placement

--- src/main/java/de/turboman/ctf/events/PlayerPlaceBlockEvent.java
+++ src/main/java/de/turboman/ctf/events/PlayerPlaceBlockEvent.java
@@ -1,6 +1,8 @@
 package de.turboman.ctf.events;
 
 import de.turboman.ctf.CaptureTheFlag;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Tag;
 import org.bukkit.event.EventHandler;
@@ -8,6 +10,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 
 public class PlayerPlaceBlockEvent implements Listener {
+    private MiniMessage mm = MiniMessage.miniMessage();
 
     @EventHandler
     public void onEvent(BlockPlaceEvent e) {
@@ -19,7 +22,20 @@ public class PlayerPlaceBlockEvent implements Listener {
             }
             case SET_FLAG -> {
                 if (Tag.BANNERS.isTagged(e.getBlock().getType())) {
+                    for (var t : CaptureTheFlag.teamList.values()) {
+                        if (t.leader() != e.getPlayer().getUniqueId()) continue;
 
+                        var loc = e.getBlock().getLocation();
+
+                        t.flagLocation(loc);
+
+                        for (var pl : t.players()) {
+                            var player = Bukkit.getPlayer(pl);
+
+                            assert player != null;
+                            player.sendMessage(mm.deserialize(CaptureTheFlag.prefix + "<green>Flag placed at <gold>" + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ()));
+                        }
+                    }
                     return;
                 }
             }
