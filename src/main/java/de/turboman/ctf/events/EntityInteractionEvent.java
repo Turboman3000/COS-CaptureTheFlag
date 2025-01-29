be95c69f92cd2ffe9af2a6cce7435b6b196a63c0
Turboman3000 <turboman@turboman.de>
2025-01-29 21:01:00 +0100

    feat(EntityInteractionEvent): Add item display on flag capture

--- src/main/java/de/turboman/ctf/events/EntityInteractionEvent.java
+++ src/main/java/de/turboman/ctf/events/EntityInteractionEvent.java
@@ -5,10 +5,16 @@ import de.turboman.ctf.GameState;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Display;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.ItemDisplay;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Transformation;
+import org.joml.AxisAngle4f;
+import org.joml.Vector3f;
 
 import java.util.UUID;
 
@@ -40,6 +46,17 @@ public class EntityInteractionEvent implements Listener {
             }
 
             CaptureTheFlag.scoreObjec.getScore("t1_" + team.id()).customName(mm.deserialize("<" + team.color() + ">" + team.name() + " <dark_gray>-<red><b> x"));
+
+            ItemDisplay itemEntity = (ItemDisplay) e.getPlayer().getLocation().getWorld().spawnEntity(e.getPlayer().getLocation(), EntityType.ITEM_DISPLAY);
+            Transformation transform = new Transformation(new Vector3f(0, 0, 0.5f), new AxisAngle4f(-0.629f, -0.777f, 0, 0), new Vector3f(1), new AxisAngle4f());
+
+            itemEntity.setTransformation(transform);
+            itemEntity.setBillboard(Display.Billboard.FIXED);
+            itemEntity.setItemDisplayTransform(ItemDisplay.ItemDisplayTransform.HEAD);
+            itemEntity.setItemStack(new ItemStack(e.getRightClicked().getLocation().getBlock().getType()));
+            itemEntity.addScoreboardTag("stolenFlag_" + team.id());
+
+            e.getPlayer().addPassenger(itemEntity);
 
             e.getRightClicked().getLocation().getBlock().setType(Material.AIR);
             e.getRightClicked().remove();
