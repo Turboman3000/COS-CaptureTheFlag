6869ebd429818d3574fd911b28bd4acf17774610
Turboman3000 <turboman@turboman.de>
2025-01-30 12:35:19 +0100

    feat(PlayerDeathEvent): Add team death handling and respawn logic

--- src/main/java/de/turboman/ctf/events/PlayerDeathEvent.java
+++ src/main/java/de/turboman/ctf/events/PlayerDeathEvent.java
@@ -4,6 +4,7 @@ import de.turboman.ctf.CaptureTheFlag;
 import de.turboman.ctf.maps.MapCursorEntry;
 import de.turboman.ctf.maps.MapManager;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -27,16 +28,55 @@ public class PlayerDeathEvent implements Listener {
 
         if (deadPlayers.contains(p.getUniqueId())) return;
 
+        int teamSize = 0;
+        int deadSize = 0;
+
         for (var t : CaptureTheFlag.teamList.values()) {
             if (!t.players().contains(p.getUniqueId())) continue;
 
+            teamSize = t.players().size();
+
             for (var pp : t.players()) {
+                if (deadPlayers.contains(pp)) {
+                    deadSize++;
+                }
+
                 MapManager.playerMaps.get(pp).cursors().put("death_" + p.getUniqueId(), new MapCursorEntry(p.getLocation().getBlockX(), p.getLocation().getBlockZ(), MapCursor.Type.TARGET_X, p.getName()));
             }
         }
 
+        deadSize++;
         deadPlayers.add(p.getUniqueId());
         CaptureTheFlag.voicechatAPI.getConnectionOf(p.getUniqueId()).setGroup(null);
+
+        if (deadSize == teamSize) {
+            for (var t : CaptureTheFlag.teamList.values()) {
+                if (!t.players().contains(p.getUniqueId())) continue;
+
+                for (var pp : t.players()) {
+                    deadPlayers.remove(pp);
+
+                    for (var ppp : t.players()) {
+                        MapManager.playerMaps.get(ppp).cursors().remove("death_" + pp);
+                    }
+
+                    CaptureTheFlag.voicechatAPI.getConnectionOf(pp).setGroup(t.voiceGroup());
+
+                    var ppp = Bukkit.getPlayer(pp);
+
+                    ppp.setGlowing(false);
+                    ppp.removePotionEffect(PotionEffectType.REGENERATION);
+                    ppp.removePotionEffect(PotionEffectType.RESISTANCE);
+                    ppp.removePotionEffect(PotionEffectType.DARKNESS);
+
+                    ppp.teleportAsync(t.flagLocation());
+
+                    p.sendMessage(mm.deserialize(prefix + "<red>Your entire Team is dead, it's getting Respawned at the Team's flag"));
+                }
+            }
+
+            return;
+        }
 
         p.sendMessage(mm.deserialize(prefix + "<red><b>You're dead!<!b><green> Wait for a Teammate to revive you!"));
         p.setGlowing(true);
